686b9b330ad6b8c12f23ff50b9b6d6557129a13a
Your Name <tony.ramirez@motionloft.com>
2011-10-10 10:30:49 -0700

    what I did

--- names.rb
+++ names.rb
@@ -1,12 +1,8 @@
-
-def name_length(names)
-  names.each do |name|
-    puts "#{name} is #{name.strip.size} chars long"
-  end
-end
-
 puts "what are the three names?"
 names = gets.chomp
 names = names.split(',')
 
-name_length(names)
+names.each do |name|
+  puts "#{name} is #{name.strip.size} chars long"
+end
+
--- number.rb
+++ number.rb
@@ -2,3 +2,5 @@ puts 'Hey man, what is your favorite number?'
 favnum= gets.chomp.to_i
 betnum= favnum + 1
 puts 'Well I like ' + betnum.to_s + ' much better, fool.'
+
+# this works!!
